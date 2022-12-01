591589d10e92a839311f1d1451f9367791886ac3
Marius <Marius.Supercode@gmail.com>
2022-12-01 15:26:33 +0100

    styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,8 +6,9 @@ import NewsElement from '../components/NewsElement';
 function HomePage() {
     const [country, setCountry] = useState("de");
     const [news, setnews] = useState([]);
-    const [api, setapi] = useState(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=8375701374204f1191e896c2d22c8309`);
+    const [api, setapi] = useState(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=a44950ea0b844b928c2605c2d8c45b4f`);
     const [error, setError] = useState("");
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         console.log("in useeffect");
@@ -17,7 +18,7 @@ function HomePage() {
                 // console.log(data);
                 setnews(data.articles);
                 setError("");
-
+                console.log(data.articles);
                 if (data.articles === undefined) {
                     setnews([]);
                     setError("Leider keine Suchergebnisse");
@@ -31,9 +32,10 @@ function HomePage() {
         <main>
             <div className="headerDiv">
                 <h1>Super News</h1>
-                <input onChange={(e) => { setnews([]); setapi(`https://newsapi.org/v2/everything?q=${e.target.value}&from=2022-12-01&sortBy=popularity&apiKey=8375701374204f1191e896c2d22c8309`); setnews([]); console.log(e.target.value); }} placeholder="Search"></input>
-                <select onChange={(e) => { setCountry(e.target.value); setapi(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=8375701374204f1191e896c2d22c8309`); }}>
-                    <option value="de">Germany</option>
+                <input onChange={(e) => setSearch(e.target.value)} placeholder="Search"></input>
+                <button onClick={(e) => { setnews([]); setapi(`https://newsapi.org/v2/everything?q=${search}&from=2022-12-01&sortBy=popularity&apiKey=a44950ea0b844b928c2605c2d8c45b4f`); setnews([]); }}>Suchen</button>
+                <select onChange={(e) => { setCountry(e.target.value); setapi(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=a44950ea0b844b928c2605c2d8c45b4f`); }}>
+                    <option value="de" >Germany</option>
                     <option value="us">America</option>
                     <option value="nl">Netherlands</option>
                 </select>
