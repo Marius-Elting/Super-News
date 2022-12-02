f93c57ce9dd053ff0922bef528691b0dc8149869
Marius <Marius.Supercode@gmail.com>
2022-12-02 20:15:39 +0100

    added pages

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,9 +6,12 @@ import NewsElement from '../components/NewsElement';
 function HomePage() {
     const [country, setCountry] = useState("de");
     const [news, setnews] = useState([]);
-    const [api, setapi] = useState(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=a44950ea0b844b928c2605c2d8c45b4f`);
-    const [error, setError] = useState("");
-    const [search, setSearch] = useState("");
+    const [error, setError] = useState();
+    const [search, setSearch] = useState();
+    const [sort, setSorting] = useState("relevancy");
+    const [api, setapi] = useState(`https://newsapi.org/v2/top-headlines?country=${country}&sortBy=${sort}&apiKey=a44950ea0b844b928c2605c2d8c45b4f`);
+    const [searchTerm, setSearchTerm] = useState("country");
+
 
     useEffect(() => {
         console.log("in useeffect");
@@ -26,17 +29,36 @@ function HomePage() {
             });
 
 
-    }, [country, api]);
+    }, [country, api, sort]);
+
+    const sortFunction = (value) => {
+        if (searchTerm === "country") {
+            console.log("A " + value);
+            setapi(`https://newsapi.org/v2/top-headlines?country=${country}&sortBy=${value}&apiKey=a44950ea0b844b928c2605c2d8c45b4f`);
+            console.log(api);
+        }
+        if (searchTerm === "search") {
+            console.log("B " + value);
+            setapi(`https://newsapi.org/v2/everything?q=${search}&from=2022-12-01&sortBy="${value}"&apiKey=a44950ea0b844b928c2605c2d8c45b4f`);
+            console.log(api);
+
+        }
+    };
 
     return (
         <main>
             <div className="headerDiv">
                 <h1>Super News</h1>
                 <div><input onChange={(e) => setSearch(e.target.value)} placeholder="Search"></input>
-                    <button onClick={(e) => { setnews([]); setapi(`https://newsapi.org/v2/everything?q=${search}&from=2022-12-01&sortBy=popularity&apiKey=a44950ea0b844b928c2605c2d8c45b4f`); setnews([]); }}>Search</button>
+                    <button onClick={(e) => { setapi(`https://newsapi.org/v2/everything?q=${search}&from=2022-12-01&sortBy="${sort}"&apiKey=a44950ea0b844b928c2605c2d8c45b4f`); setSearchTerm("search"); }}>Search</button>
                 </div>
-
-                <select onChange={(e) => { setCountry(e.target.value); setapi(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=a44950ea0b844b928c2605c2d8c45b4f`); }}>
+                <select onChange={(e) => { setSorting(e.target.value); sortFunction(e.target.value); }}>
+                    <option value={`relevancy`}>Funktioniert noch nicht</option>
+                    <option value={`relevancy`}>Relevancy</option>
+                    <option value={`popularity`}>Popularity</option>
+                    <option value={`PublishedAt`}>PublishedAt</option>
+                </select>
+                <select onChange={(e) => { setCountry(e.target.value); setapi(`https://newsapi.org/v2/top-headlines?country=${e.target.value}&sortBy=${sort}&apiKey=a44950ea0b844b928c2605c2d8c45b4f`); setSearchTerm("country"); }}>
                     <option value="de" >Germany</option>
                     <option value="us">America</option>
                     <option value="nl">Netherlands</option>
@@ -50,7 +72,7 @@ function HomePage() {
                     );
                 })}
             </div>
-        </main>
+        </main >
     );
 }
 
